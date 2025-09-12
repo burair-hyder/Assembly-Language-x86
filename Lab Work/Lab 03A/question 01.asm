e16b09359298269e69f623de307460712c046dde
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-09-12 23:21:30 +0500

    Update question 01.asm

--- Lab Work/Lab 03A/question 01.asm
+++ Lab Work/Lab 03A/question 01.asm
@@ -1,1 +1,34 @@
+INCLUDE Irvine32.inc
 
+;SYED MUHAMMAD BURAIR HYDER
+;24K-0804
+
+.data
+	aVal DWORD 10h
+	bVal DWORD 12h
+	cVal DWORD 20h
+	dVal DWORD 25h
+
+.code 
+
+
+main PROC
+
+	mov ecx,aVal
+	add ecx,bVal
+
+	mov eax,aVal
+	sub eax,bVal
+
+	sub ecx,eax
+	add ecx,cVal
+	add ecx,dVal
+
+	mov ebx,ecx
+
+	Call DumpRegs
+
+exit 
+
+main ENDP
+END main
