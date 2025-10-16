ef66d2a7ac1f8fa41fb573115d07e85e1c5da7d2
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-10-16 23:02:31 +0500

    Update Randomize.asm

--- Lab Work/Lab 06 (builtIn Procedures)/Randomize.asm
+++ Lab Work/Lab 06 (builtIn Procedures)/Randomize.asm
@@ -1,1 +1,13 @@
+INCLUDE Irvine32.inc
+
+.code
+main PROC
+    call Randomize        ; seed random generator
+    mov  eax, 100         ; range = 0–99
+    call RandomRange      ; eax = random number < 100
+    call WriteDec         ; print it
+    call Crlf
+    exit
+main ENDP
+END main
 
