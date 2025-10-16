20a9ab9d28d40b36cb479eaae58a905f706c5de3
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-10-16 23:12:05 +0500

    Update jz & jnz Jump.asm

--- Lab Work/Lab 06 (Examples)/jz & jnz Jump.asm
+++ Lab Work/Lab 06 (Examples)/jz & jnz Jump.asm
@@ -1,7 +1,7 @@
 INCLUDE Irvine32.inc
 ;SYED MUHAMMAD BURAIR HYDER 24K-0804
 .data
-	val1 DWORD 100
+	val1 DWORD 200
 	val2 DWORD 100
 	msg1 BYTE "Both are Equal",0
 	msg2 BYTE "Both are Not Equal",0
@@ -9,21 +9,22 @@ INCLUDE Irvine32.inc
 .code 
 main PROC
 	mov eax,val1
-	mov ebx,val1
+	mov ebx,val2
 	cmp eax,ebx
 	jz Zero
 	jnz notZero
 
 
-	Zero:
+Zero:
 		mov edx,OFFSET msg1
 		Call WriteString
 		Call exitt
-	notZero:
+notZero:
 		mov edx,OFFSET msg2
 		Call WriteString
+		Call exitt
 
-	exitt:
+exitt:
 
 exit 
 main ENDP
