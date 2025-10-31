38804b1aa25b64cf65847e708783c5e3d74701ef
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-11-01 00:18:50 +0500

    Create question 06.asm

---
+++ Assignment-2/question 06.asm
@@ -0,0 +1,59 @@
+INCLUDE Irvine32.inc
+
+
+.data
+	
+	op1 DWORD 15
+	op2 DWORD 20
+	x DWORD 5
+	y DWORD 12
+	z DWORD 0
+
+.code
+
+main PROC
+
+	whileloop:
+
+		mov eax,x
+		cmp op1,eax
+		jge con2
+		sub z,10
+
+		
+
+		cont:
+
+		dec op1
+
+		mov ebx,op2
+		cmp op1,ebx
+		jle whileloop
+		jmp exitt
+
+
+con2:
+	mov eax,y
+	cmp op1,eax
+	jle summ
+	sub z,10
+	jmp cont
+	
+
+
+summ:
+	add z,10
+	jmp cont
+
+
+
+
+
+exitt:
+	mov eax,z
+	Call WriteInt
+	Call Crlf
+		
+exit
+main ENDP
+END main
